dac69ee8bc34ccbf9288684096ea22936d70c4c6
Forrest Benton <elliottbenton@gmail.com>
2018-06-21 20:06:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,19 +16,20 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.each do |element|
-  windex_1 = element[0]
-  windex_2 = element[1]
-  windex_3 = element[2]
+    windex_1 = element[0]
+    windex_2 = element[1]
+    windex_3 = element[2]
   
-  position_1 = board(windex_1)
-  position_2 = board(windex_2)
-  position_3 = board(windex_3)
+    position_1 = board(windex_1)
+    position_2 = board(windex_2)
+    position_3 = board(windex_3)
   
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return [windex_1,windex_2,windex_3]
-  else 
-    false 
-  end 
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return [windex_1,windex_2,windex_3]
+    else 
+      false 
+    end 
+  end
 end
   
   
